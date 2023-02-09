3571be2396723d1c3d5e0af046c3fe4f518ce0f5
AZAFARANI <zafaranian8@gmail.com>
2023-02-09 10:16:07 +0100

    added login and get users routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ const UserModel = require("../models/userModel");
 const express = require("express");
 const userRouter = express.Router();
 const jwt = require("jsonwebtoken");
+const { hashedPassword, comparePassword } = require("../utils");
 
 userRouter.get("/hello", (req, res) => {
   res.send("hello");
@@ -22,19 +23,84 @@ userRouter.post("/register", async (req, res) => {
       const newUser = new UserModel({
         userName,
         email,
-        password,
+        password: hashedPassword(password),
         registered: Date.now().toString(),
       });
-      const userData = {
-        username: userName,
-        email: email,
-      };
+
       await newUser.save();
-      const accessToken = jwt.sign(userData, process.env.JWT_SECRET);
-      res.send(accessToken);
-      console.log(accessToken);
+      const token = jwt.sign({ _id: newUser._id }, process.env.JWT_SECRET);
+
+      res.send({ token, user: newUser.userName });
     }
   });
 });
+
+userRouter.post("/login", async (req, res) => {
+  const username = req.body.userName;
+  const password = req.body.password;
+
+  try {
+    const user = await UserModel.findOne({ userName: username });
+
+    const isMatch = comparePassword(password, user.password);
+
+    if (isMatch === true) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "24h",
+      });
+
+      return res.json({
+        success: true,
+        message: "Authentication successful!",
+        token: token,
+        user: user.userName,
+      });
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: "Incorrect username or password",
+      });
+    }
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      message: "Incorrect username or password",
+    });
+  }
+});
+
+userRouter.get("/users", verifyToken, async (req, res) => {
+  try {
+    // Find all users in the database
+    const users = await UserModel.find({}).lean();
+
+    // Send users to client
+    return res.json({
+      success: true,
+      users: users,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Error getting users",
+    });
+  }
+});
+
+// Middleware function to verify JWT
+function verifyToken(req, res, next) {
+  const token = req.header("auth-token");
+  if (!token) return res.status(401).send("Access Denied");
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      message: "access denied!",
+    });
+  }
+}
 
 module.exports = userRouter;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Cookies from "js-cookie";
 
 function App() {
   const go = () => {
     let hej = {
-      userName: "hassan2",
-      email: "zaf2",
+      userName: "piramm",
+
       password: "hello123",
-      confirmPassword: "hello123",
     };
 
-    fetch("http://localhost:8000/register", {
-      method: "POST", // or 'PUT'
+    fetch("http://localhost:8000/users", {
+      method: "GET", // or 'PUT'
       headers: {
-        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZTRiMjk2ZGUxNjM0MGU1NzFjOTMyMCIsImlhdCI6MTY3NTkzMzM5NSwiZXhwIjoxNjc2MDE5Nzk1fQ.77tMOjFUZHNlCFlxui86Bq9XAyfZtfDRMH6QXUM9IWg",
       },
-      body: JSON.stringify(hej),
-    }).then((res) => console.log(res));
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      });
   };
   return (
     <div className="App">
